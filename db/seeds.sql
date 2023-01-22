3514ee494c983f2c336b699f2d3cdba5a0b06ea3
Sam O'Cain <samocain@Sams-MBP.lan>
2023-01-21 22:17:57 -0600

    changes post tutoring

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES ('Engineering'), ('Finance'), ('Legal'), ('Sales');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Sales Manager', 150000, 4),
+    ('Inside Sales', 70000, 4),
+    ('General Counsel', 200000, 3),
+    ('Software Engineer', 100000, 1),
+    ('Accountant', 80000, 2),
+    ('CFO', 250000, 2);
+
+    INSERT INTO employee (first_name, last_name, role_id)
+        VALUES ('John', 'David', 2),
+        ('William', 'Johnson', 4),
+        ('James', 'Kelly', 1),
+        ('Tim', 'Riggins', 3),
+        ('Michael', 'Scott', 5),
+        ('David', 'Wallace', 6);
+        
+
--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection(
     {
-        host: 'localhost',
+        host: '127.0.0.1',
+
         user: 'root',
         password: 'GoNole$0533',
         database: 'employees_db'
@@ -17,7 +18,8 @@ const db = mysql.createConnection(
 
     db.connect((err) => {
         if (err) throw err;
-        console.log('Connected to the database successfully!')
+        console.log('Connected to the database successfully!');
+        init();
     })
 
 
@@ -30,4 +32,49 @@ const db = mysql.createConnection(
             message: "Let's Get Started: ",
             choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
         }
+    ];
+
+    const addDepartmentQuestion = [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please create a name for your new department'
+        }
     ]
+
+
+    // START APP //
+
+    function init() {
+        inquirer.prompt(question).then((response) => {
+            if (response.choice === 'View All Employees') {
+                viewEmployees();
+            } else if (response.choice === 'Add Employee') {
+                addEmployee();
+            } else if (response.choice === 'View All Departments'){
+                viewDepartments();
+            } else if (response.choice === 'Add Department'){
+                addDepartment();
+            }
+        })
+    }
+
+    
+
+
+    function viewDepartments() {
+        db.query('SELECT * FROM department;', (err, data) => {
+            console.table(data);
+            init()
+        })
+    }
+
+
+    function addDepartment() {
+        inquirer.prompt(addDepartmentQuestion).then((response) => {
+            db.query('INSERT INTO department (name) VALUES (?)', [response.name], (err) => {
+                viewDepartments();
+            })
+        })
+        
+    }
