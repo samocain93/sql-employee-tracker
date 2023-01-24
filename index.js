b9bda4a240f645eb36da7b95b7fdc380b468757d
Sam O'Cain <samocain@Sams-MBP.lan>
2023-01-24 13:11:50 -0600

    new updates from tutoring session

--- index.js
+++ index.js
@@ -47,6 +47,68 @@ const db = mysql.createConnection(
         }
     ]
 
+    const addRoleQuestion = [
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Please enter a title for the new role'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'Please enter the salary for the new role'
+        },
+        {
+            type: 'list',
+            name: 'id',
+            message: 'Please enter the corresponding Department ID for the new role',
+            choices: []
+        },
+
+    ]
+
+    const addEmployeeQuestion = [
+        {
+            type: 'input',
+            name: 'firstName',
+            message: 'Please enter the first name of the new employee'
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'Please enter the last name of the new employee'
+        },
+        {
+            type: 'list',
+            name: 'roleId',
+            message: 'Please select the role for the new employee',
+            choices: []
+        },
+        {
+            type: 'list',
+            name: 'managerId',
+            message: 'Please select the manager for the new employee',
+            choices: []
+        },
+
+    ]
+
+    const updateEmployeeQuestion = [
+        {
+            type: 'list',
+            name: 'employee',
+            message: 'Please select which employee you would like to update',
+            choices: []
+        },
+        {
+            type: 'list',
+            name: 'newTitle',
+            message: 'Please select the new role for this employee',
+            choices: []
+        }
+    ]
+
+
 
     // ----- START APP FUNCTION ----- //
 
@@ -101,6 +163,40 @@ const db = mysql.createConnection(
         
     };
 
+    // -----  ADD ROLE -----  // 
+
+    function addRole() {
+        db.query('SELECT name, id AS value FROM department;', (err, data) => {
+            addRoleQuestion[2].choices = data;
+            inquirer.prompt(addRoleQuestion).then((response) => {
+                db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [response.title, response.salary, response.id], (err) => {
+                    viewRoles();
+                })
+            })
+        })
+        
+    }
+
+    //  ----- ADD EMPLOYEE -----  //
+
+    function addEmployee() {
+       
+        db.query('SELECT title AS name, id AS value FROM role;', (err, data) => {
+            db.query('SELECT CONCAT(first_name, " ", last_name) AS name, id AS value FROM employee', (err, employeeData) => {
+                addEmployeeQuestion[2].choices = data;
+                addEmployeeQuestion[3].choices = employeeData;
+                inquirer.prompt(addEmployeeQuestion).then((response) => {
+                    db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)' , [response.firstName, response.lastName, response.roleId, response.managerId], (err) => {
+                        viewEmployees()
+                    })
+                })
+               
+            })
+        })
+
+
+    };
+
 
 
 // ----- VIEW ALL EMPLOYEES ----- //
@@ -115,13 +211,7 @@ const db = mysql.createConnection(
     };
 
 
-//  ----- ADD EMPLOYEE -----  //
 
-    function addEmployee() {
-        
-
-
-    };
 
 
 
@@ -129,6 +219,18 @@ const db = mysql.createConnection(
 
     function updateEmployee() {
 
+        db.query('SELECT title AS name, id AS value FROM role;', (err, data) => {
+            db.query('SELECT CONCAT(first_name, " ", last_name) AS name, id AS value FROM employee', (err, employeeData) => {
+                updateEmployeeQuestion[1].choices = data;
+                updateEmployeeQuestion[0].choices = employeeData;
+                inquirer.prompt(updateEmployeeQuestion).then((response) => {
+                    db.query('UPDATE employee SET role_id=? WHERE id=?', [response.newTitle, response.employee], (err) => {
+                        viewEmployees()
+                    })
+                })
+               
+            })
+        })
 
 
     };
@@ -137,22 +239,22 @@ const db = mysql.createConnection(
 //  ----- VIEW ALL ROLES -----  //
 
     function viewRoles() {
+        db.query('SELECT * FROM role;', (err, data) => {
+          
+            if (err) throw (err)
+            console.table(data)
+            init()
+        
+        })
+
+
+        };
 
 
 
-    };
 
 
 
-
-
-// -----  ADD ROLE -----  // 
-
-    function addRole() {
-
-
-
-    }
 
 
 
